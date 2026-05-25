3b38a68628ae679ba5eb895def80469db169efdd
Nikko Le <nikster731@gmail.com>
2026-05-24 23:23:38 -0700

    Fix gas simulation initialization and wall collisions

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ from concurrent.futures import ThreadPoolExecutor
 @jit(nogil=True)
 def simulate(t):
   # use local variables because stack accesses are faster than reading from global
-  l, n, m, k, pi = .005, 4194304, 1.008 / (6.02214e23), 1.380649e-23, 3.141592653589793
+  l, n, m, k, pi = .005, 65536, 1.008 / (6.02214076e23), 1.380649e-23, 3.141592653589793
   
   r = np.zeros((n, 3))
   v = np.zeros((n, 3))
   
+  sigma = math.sqrt(k * t / m)
+
   for i in range(n):
-    theta = rnd.uniform(-pi, pi)
-    phi = rnd.uniform(-pi / 2, pi / 2)
-    x, y, z = math.cos(theta) * math.cos(phi), math.sin(theta) * math.cos(phi), math.sin(phi)
-    v[i] = np.array([x, y, z]) / math.sqrt(x**2 + y**2 + z**2) * rnd.normal(0, math.sqrt(k*t/m))
-    r[i] = np.array([rnd.uniform(0, l), rnd.uniform(0, l), rnd.uniform(0, l)])
+    r[i, 0] = rnd.uniform(0, l)
+    r[i, 1] = rnd.uniform(0, l)
+    r[i, 2] = rnd.uniform(0, l)
+
+    v[i, 0] = rnd.normal(0, sigma)
+    v[i, 1] = rnd.normal(0, sigma)
+    v[i, 2] = rnd.normal(0, sigma)
   
   avg_vel = np.sum(v, axis=0) / n
   for i in range(n):
@@ -40,7 +44,7 @@ def simulate(t):
 
 @jit(nogil=True)
 def loop(r, v, m, l, t):
-  dt = 5e-6
+  dt = 1e-6
   n = 0
   total = 0
   n_iters = 1024
@@ -50,15 +54,33 @@ def loop(r, v, m, l, t):
     j1, j2, j3 = 0, 0, 0
     for i in range(len(r)):
       r[i] += v[i] * dt
-      a0 = r[i][0] <= 0 or r[i][0] >= l
-      a1 = r[i][1] <= 0 or r[i][1] >= l
-      a2 = r[i][2] <= 0 or r[i][2] >= l
-      j1 += 2 * m * math.fabs(v[i][0]) * a0
-      j2 += 2 * m * math.fabs(v[i][1]) * a1
-      j3 += 2 * m * math.fabs(v[i][2]) * a2
-      v[i][0] *= a0 * -2 + 1
-      v[i][1] *= a1 * -2 + 1
-      v[i][2] *= a2 * -2 + 1
+
+      if r[i][0] < 0:
+        j1 += 2 * m * math.fabs(v[i][0])
+        r[i][0] = -r[i][0]
+        v[i][0] *= -1
+      elif r[i][0] > l:
+        j1 += 2 * m * math.fabs(v[i][0])
+        r[i][0] = 2 * l - r[i][0]
+        v[i][0] *= -1
+
+      if r[i][1] < 0:
+        j2 += 2 * m * math.fabs(v[i][1])
+        r[i][1] = -r[i][1]
+        v[i][1] *= -1
+      elif r[i][1] > l:
+        j2 += 2 * m * math.fabs(v[i][1])
+        r[i][1] = 2 * l - r[i][1]
+        v[i][1] *= -1
+
+      if r[i][2] < 0:
+        j3 += 2 * m * math.fabs(v[i][2])
+        r[i][2] = -r[i][2]
+        v[i][2] *= -1
+      elif r[i][2] > l:
+        j3 += 2 * m * math.fabs(v[i][2])
+        r[i][2] = 2 * l - r[i][2]
+        v[i][2] *= -1
   
     p = (j1 + j2 + j3) / surface_area / dt
     total += p
@@ -76,7 +98,7 @@ def main():
       f.write(repr(it) + '\n')
 
   fig = plt.figure()
-  x = np.array([4194304 / 6.02214076e23 * temp for temp in result_map.keys()]).reshape(-1, 1)
+  x = np.array([65536 / 6.02214076e23 * temp for temp in result_map.keys()]).reshape(-1, 1)
   y = np.array([0.000000125 * pressure for pressure in result_map.values()]).reshape(-1, 1)
   reg = LinearRegression().fit(x, y)
   plt.plot(x, y, 'bo')
